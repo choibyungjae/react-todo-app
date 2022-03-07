fd6998d288b8f493298d459f14c0fd86a684abd7
Choi Byung Jae <39176639+choibyungjae@users.noreply.github.com>
2022-03-07 09:53:47 +0900

    todoList

---
+++ react-todo-app/src/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import { RecoilRoot } from "recoil";
+import { ThemeProvider } from "styled-components";
+import { darkTheme } from "./theme";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <RecoilRoot>
+    <ThemeProvider theme={darkTheme}>
+    <App />
+      </ThemeProvider>
+      </RecoilRoot>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
