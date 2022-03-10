7760a228bbc80a90a1f81c779ae4df1160e0fced
Choi Byung Jae <39176639+choibyungjae@users.noreply.github.com>
2022-03-10 11:29:38 +0900

    20220310

--- react-todo-app/src/App.tsx
+++ react-todo-app/src/App.tsx
@@ -1,6 +1,5 @@
 import { createGlobalStyle } from "styled-components";
-import ToDoList from "./ToDoList";
-
+import ToDoList from "./components/ToDoList";
 
 const Globalstyle = createGlobalStyle`
 @import url('https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@300;400&display=swap');
